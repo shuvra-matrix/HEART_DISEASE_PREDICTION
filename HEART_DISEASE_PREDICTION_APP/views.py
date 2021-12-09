6dba38f5a98ac76c8e068b48c23d6844d23fca29
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-12-09 18:56:53 +0530

    update

--- HEART_DISEASE_PREDICTION_APP/views.py
+++ HEART_DISEASE_PREDICTION_APP/views.py
@@ -1,5 +1,13 @@
+import joblib
 from django.shortcuts import render
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # Create your views here.
 def index(request):
+    
+    project = joblib.load("heart_model.sav")
+    list = [57,0,0,140,241,0,1,123,1,0.2,1,0,3]
+    ans = project.predict([list])
+    print(ans)
     return render(request, "index.html")
