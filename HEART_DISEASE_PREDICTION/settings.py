313a222526b688e76d40d3602741622b90d3deba
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-12-10 06:22:58 +0530

    Update settings.py

--- HEART_DISEASE_PREDICTION/settings.py
+++ HEART_DISEASE_PREDICTION/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = [STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
