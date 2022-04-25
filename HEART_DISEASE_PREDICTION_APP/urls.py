77bb121a5569e5455f2d80df4a04acf71db893e1
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-04-25 23:33:35 +0530

    prediction 2.0 done

--- HEART_DISEASE_PREDICTION_APP/urls.py
+++ HEART_DISEASE_PREDICTION_APP/urls.py
@@ -7,4 +7,5 @@ app_name = "pre"
 urlpatterns = [
     path('',views.index,name="index" ),
     path("heart/",views.heart_disease,name="heart"),
+    path("heart2/",views.heart_prediction_2,name="heart2"),
 ]
