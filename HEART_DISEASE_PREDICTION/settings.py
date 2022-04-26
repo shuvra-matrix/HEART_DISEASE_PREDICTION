d7291fa0dc42efa97ad2bb1f5b4ef30f08ff33a2
SHUVRA CHAKRABARTY <shuvrachakrabarty97@gmail.com>
2022-04-26 09:46:48 +0530

    remove pwa

--- HEART_DISEASE_PREDICTION/settings.py
+++ HEART_DISEASE_PREDICTION/settings.py
@@ -132,34 +132,5 @@ STATIC_ROOT = STATIC_DIR
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-PWA_APP_NAME = 'Arrow Health'
-PWA_APP_DESCRIPTION = "Heart Disease Prediction"
-PWA_APP_THEME_COLOR = '#000000'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_STATUS_BAR_COLOR = 'default'
-PWA_APP_ICONS = [
-	{
-		'src': 'static/images/arrow.png',
-		'sizes': '160x160'
-	}
-]
-PWA_APP_ICONS_APPLE = [
-	{
-		'src': 'static/images/arrow.png',
-		'sizes': '160x160'
-	}
-]
-PWA_APP_SPLASH_SCREEN = [
-	{
-		'src': 'static/images/arrow.png',
-		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-	}
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js') 
+
 
