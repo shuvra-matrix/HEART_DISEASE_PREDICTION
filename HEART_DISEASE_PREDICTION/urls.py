fafa4eab183739b24ff7f84d2c61de8767ffacfc
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-02 03:30:34 +0530

    update

--- HEART_DISEASE_PREDICTION/urls.py
+++ HEART_DISEASE_PREDICTION/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("HEART_DISEASE_PREDICTION_APP.urls")),
-   	path('', include('pwa.urls')),
+    path('', include("HEART_DISEASE_PREDICTION_APP.urls")),
+    path('', include('pwa.urls')),
 
 ]
